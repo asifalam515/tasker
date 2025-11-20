cff2e90ddfd38cfac103c54ad45ee0c5ff86271b
asifalam515 <asibulalam515@gmail.com>
2025-11-20 09:30:01 +0600

    hero section updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl">Project started</h1>
       <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
     </div>
   );
 };
