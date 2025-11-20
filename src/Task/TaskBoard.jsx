07bc38d9bd9b0dd55e2c53bd1baf98966f7c936a
asifalam515 <asibulalam515@gmail.com>
2025-11-20 23:41:37 +0600

    nothing new

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -21,6 +21,7 @@ const TaskBoard = () => {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           {/* task action */}
+         
           <TaskActions></TaskActions>
           <TaskList tasks={tasks}></TaskList>
         </div>
