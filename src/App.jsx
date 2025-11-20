5a9f756221a7e8c2a388c04f4c6dc376e3badd74
asifalam515 <asibulalam515@gmail.com>
2025-11-20 11:20:57 +0600

    task list component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./Task/TaskBoard";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
-      <HeroSection></HeroSection>
+      <div className="flex flex-col  justify-center items-center ">
+        {" "}
+        <HeroSection></HeroSection>
+        <TaskBoard></TaskBoard>
+      </div>
       <Footer></Footer>
     </div>
   );
