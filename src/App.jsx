ce2070c30292cb24d1206067bf628ecf2636dfae
asifalam515 <asibulalam515@gmail.com>
2025-11-20 09:18:58 +0600

    heaer section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-5xl">Project started</h1>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default App;
