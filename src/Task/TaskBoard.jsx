a1601e368573881bb988f76cc6234a6031013de6
asifalam515 <asibulalam515@gmail.com>
2025-11-21 12:53:46 +0600

    handler added

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useActionState, useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
+
   const defaultTask = {
     id: crypto.randomUUID(),
     title: "Learn React",
@@ -13,8 +15,16 @@ const TaskBoard = () => {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal,setShowAddModal] = useState(false)
+    // for handling task
+   const handleAddTask=()=>{
+setShowAddModal(true)
+  }
   return (
     <section className="mb-20" id="tasks">
+      {
+        showAddModal && <AddTaskModal></AddTaskModal>
+      }
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask></SearchTask>
@@ -22,7 +32,7 @@ const TaskBoard = () => {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           {/* task action */}
          
-          <TaskActions></TaskActions>
+          <TaskActions handleAddTask={()=>setShowAddModal(true)}></TaskActions>
           <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
